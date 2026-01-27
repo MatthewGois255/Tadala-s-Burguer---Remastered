372da677c0365d12b0588ecb2b77b5f12ff00b1b
Matheus Gois <159056295+MatthewGois255@users.noreply.github.com>
2026-01-27 11:55:38 -0300

    Add files via upload

---
+++ wdev/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use \App\Http\Router;
+
+
+
+$obj = new Router("");
+print_r($obj);
+
+echo "hello";
---
+++ wdev/src/Http/Router.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http;
+
+class Router {
+    private $url = '';
+    private $prefix = '';
+    private $routes = [];
+    private $request;
+
+    public function __construct($url) {
+        $this->request = new Request();
+        $this->url = $url;
+        $this->setPrefix();
+    }
+
+    private function setPrefix() {
+        $parseUrl = parse_url($this->url);
+        print_r($parseUrl);
+        exit;
+    }
+}
